588cc7f8a87836d97e561ffe9874cb05fcad7860
Jared Folkins <jfolkins@gmail.com>
2015-01-13 15:07:24 -0800

    enhc: added more tests for studio

--- studio_test.go
+++ studio_test.go
@@ -19,10 +19,42 @@ func TestStudioAddRule(t *testing.T) {
 		Sentence:    time.Millisecond * 10,
 	}
 
+	// add rule
 	st.AddRule(r)
 
+	// test rule exists
 	_, ok := st.rules[r.Name]
 	if ok == false {
 		t.Errorf("AddRule for Actor [%s] should not fail", an)
 	}
+}
+
+func TestStudioAddRules(t *testing.T) {
+	st := NewStudio(256)
+	an := "an_" + strconv.FormatInt(time.Now().UnixNano(), 10)
+	rn := "rn_" + strconv.FormatInt(time.Now().UnixNano(), 10)
+	rm := "rm_" + strconv.FormatInt(time.Now().UnixNano(), 10)
+	r := &Rule{
+		Name:        rn,
+		Message:     rm,
+		StrikeLimit: 3,
+		ExpireBase:  time.Millisecond * 10,
+		Sentence:    time.Millisecond * 10,
+	}
+
+	// add rule safety is of no concern
+	st.rules[r.Name] = r
+
+	// apply rules
+	st.ApplyRules()
+
+	for _, r := range st.rules {
+		for _, d := range st.directors {
+			_, ok := d.rules[r.Name]
+			if !ok {
+				t.Errorf("ApplyRules for Actor [%s] should not fail", an)
+			}
+		}
+	}
+
 }
